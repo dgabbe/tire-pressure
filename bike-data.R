de33494242af245460956db3c0ec86d809ae83dd
David G <dgabbe@acm.org>
2015-10-13 22:59:50 -0400

    Added L's bike.

--- bike-data.R
+++ bike-data.R
@@ -35,6 +35,15 @@ norco_fun_bike <- bike_tire_pressures(rider_weight_lbs = 165,
 
 norco_plot <- display_bike_inflation(base_inflation_plot, norco_shopping_bike)
 
+leslie_trek_lexa_bike <- bike_tire_pressures(rider_weight_lbs = 145,
+                                      bike_weight_lbs = 15,
+                                      load_lbs = 1,
+                                      front_distribution = 0.4,
+                                      front_tire_size_mm = 28,
+                                      rear_tire_size_mm = 26)
+
+leslie_trek_lexa_plot <- display_bike_inflation(base_inflation_plot, leslie_trek_lexa_bike)
+
 # Someone like Nairo Quintana
 light_rider_bike <- bike_tire_pressures(
   rider_weight_lbs = 125,
