56c5221229b9f664be064d4a615ed39d33db7026
David Gabbé <dgabbe@acm.org>
2017-08-17 07:56:52 -0400

    Reformatting - will break diffs; tweaks to sibebarPanel to improve looks/usability; removal of example comments.

--- tire_pressure_shiny/server.R
+++ tire_pressure_shiny/server.R
@@ -8,30 +8,21 @@ tire_casing_indicator <- function(checkbox) {
   )
 }
 
-# Define server logic required to draw a histogram
 shinyServer(
   function(input, output) {
-
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-
-  output$inflation_plot <- renderPlot({
-    bike <- bike_tire_pressures(
-      rider_weight_lbs = input$rider_weight,
-      bike_weight_lbs = input$bike_weight,
-      load_lbs = input$load_weight,
-      front_distribution = (input$front_distribution / 100),
-      front_tire_size_mm = as.numeric(input$front_tire),
-      front_tire_casing_compensation = tire_casing_indicator(input$front_casing_extralight),
-      rear_tire_size_mm = as.numeric(input$rear_tire),
-      rear_tire_casing_compensation = tire_casing_indicator(input$rear_casing_extralight)
-    )
-    plot_bike_inflation(bike = bike)
-  },
-  height = 800)
+    output$inflation_plot <- renderPlot({
+      bike <- bike_tire_pressures(
+        rider_weight_lbs = input$rider_weight,
+        bike_weight_lbs = input$bike_weight,
+        load_lbs = input$load_weight,
+        front_distribution = (input$front_distribution / 100),
+        front_tire_size_mm = as.numeric(input$front_tire),
+        front_tire_casing_compensation = tire_casing_indicator(input$front_casing_extralight),
+        rear_tire_size_mm = as.numeric(input$rear_tire),
+        rear_tire_casing_compensation = tire_casing_indicator(input$rear_casing_extralight)
+      )
+      plot_bike_inflation(bike = bike)
+    },
+    height = 800)
   }
 )
--- tire_pressure_shiny/ui.R
+++ tire_pressure_shiny/ui.R
@@ -2,8 +2,8 @@ library(btpress)
 library(shiny)
 # library(shinythemes)
 
-slider_width <- "250px"
-dropdown_width <- "125px"
+slider_width <- "280px"
+dropdown_width <- "50px"
 
 shinyUI(
   fluidPage( # theme = shinytheme("yeti"),
@@ -15,26 +15,32 @@ shinyUI(
       sidebarPanel(
         width = 3,
         fluidRow(
-          column(12, h4("Bicycle and Rider Information", align = "center"))),
+          column(12, h4("Bicycle and Rider Information", align = "center"))
+        ),
         fluidRow(
-          column(12, helpText("Use at your own risk."))),
+          column(12, helpText("Use at your own risk."))
+        ),
         fluidRow(
-          column(12,
-          sliderInput("rider_weight", label = "Rider Weight:", min = 80, max = 250, value = c(150), width = slider_width))
-          ),
+          column(
+            12,
+            sliderInput("rider_weight", label = "Rider Weight:", min = 80, max = 250, value = c(150), width = slider_width))
+        ),
         fluidRow(
-          column(12,
-          sliderInput("bike_weight", label = "Bike Weight:", min = 10, max = 50, value = c(15), width = slider_width))
-          ),
+          column(
+            12,
+            sliderInput("bike_weight", label = "Bike Weight:", min = 10, max = 50, value = c(15), width = slider_width))
+        ),
         fluidRow(
-          column(12,
-          sliderInput("load_weight", label = "Load Weight:", min = 0, max = 75, value = c(13), width = slider_width))
-          ),
+          column(
+            12,
+            sliderInput("load_weight", label = "Load Weight:", min = 0, max = 72, value = c(13), width = slider_width))
+        ),
         fluidRow(
-          column(12,
-          sliderInput("front_distribution", label = "Front Wheel Load (%):",
-                      min = 35, max = 70, step = 5, value = c(40), width = slider_width))
-          ),
+          column(
+            12,
+            sliderInput("front_distribution", label = "Front Wheel Load (%):",
+                        min = 35, max = 70, step = 5, value = c(40), width = slider_width))
+        ),
         fluidRow(
           column(
             12,
@@ -43,26 +49,29 @@ shinyUI(
                 column(12, h5('Tire Sizes (mm)', align = "left", style="font-weight: bold;"))
               ),
               fluidRow(
-                column(5,
-                       selectInput("front_tire", label = "Front:",
-                                   choices = btpress::tire_sizes_mm, selected = 28,
-                                   width = dropdown_width, selectize = FALSE)),
-                column(7,
-                       checkboxInput("front_casing_extralight", "Extra Light Casing", FALSE))
+                column(
+                  4,
+                  selectInput("front_tire", label = "Front:",
+                              choices = btpress::tire_sizes_mm, selected = 28,
+                              width = dropdown_width, selectize = FALSE)
+                ),
+                column(
+                  8,
+                  checkboxInput("front_casing_extralight", "Extra Light Casing", FALSE))
               ),
               fluidRow(
                 column(
-                  5,
+                  4,
                   selectInput("rear_tire", label = 'Rear:',
                               choices = btpress::tire_sizes_mm, selected = 28,
                               width = dropdown_width, selectize = FALSE)),
                 column(
-                  7,
+                  8,
                   checkboxInput("rear_casing_extralight", "Extra Light Casing", FALSE)
                 )
               )
-            )
-          ) # wellPanel
+            ) # wellPanel
+          )
         )
       ),
       mainPanel(width = 9, plotOutput("inflation_plot"))
