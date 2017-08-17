7fd956edde7ad15c9f6138ac8744e293e92e4285
David Gabbé <dgabbe@acm.org>
2017-08-17 08:09:32 -0400

    Added wells for better grouping and created functions for better UI grouping.

--- tire_pressure_shiny/ui.R
+++ tire_pressure_shiny/ui.R
@@ -5,6 +5,71 @@ library(shiny)
 slider_width <- "280px"
 dropdown_width <- "50px"
 
+tire_well <- function() {
+  wellPanel(
+    fluidRow(
+      column(12, h5('Tire Sizes (mm)', align = "left", style="font-weight: bold;"))
+    ),
+    fluidRow(
+      column(
+        4,
+        selectInput("front_tire", label = "Front:",
+                    choices = btpress::tire_sizes_mm, selected = 28,
+                    width = dropdown_width, selectize = FALSE)
+      ),
+      column(
+        8,
+        checkboxInput("front_casing_extralight", "Extra Light Casing", FALSE))
+    ),
+    fluidRow(
+      column(
+        4,
+        selectInput("rear_tire", label = 'Rear:',
+                    choices = btpress::tire_sizes_mm, selected = 28,
+                    width = dropdown_width, selectize = FALSE)),
+      column(
+        8,
+        checkboxInput("rear_casing_extralight", "Extra Light Casing", FALSE)
+      )
+    )
+  )
+}
+
+bike_well <- function() {
+  wellPanel(
+    fluidRow(
+      column(
+        12,
+        sliderInput("bike_weight", label = "Bike Weight:", min = 10, max = 50, value = c(15), width = slider_width)
+      )
+    ),
+    fluidRow(
+      column(
+        12,
+        sliderInput("load_weight", label = "Load Weight:", min = 0, max = 72, value = c(13), width = slider_width)
+      )
+    ),
+    fluidRow(
+      column(
+        12,
+        sliderInput("front_distribution", label = "Front Wheel Load (%):",
+                    min = 35, max = 70, step = 5, value = c(40), width = slider_width)
+      )
+    )
+  )
+}
+
+rider_well <- function() {
+  wellPanel(
+    fluidRow(
+      column(
+        12,
+        sliderInput("rider_weight", label = "Rider Weight:", min = 80, max = 250, value = c(150), width = slider_width)
+      )
+    )
+  )
+}
+
 shinyUI(
   fluidPage( # theme = shinytheme("yeti"),
     tags$head(includeScript("google-analytics.js")),
@@ -20,57 +85,12 @@ shinyUI(
         fluidRow(
           column(12, helpText("Use at your own risk."))
         ),
+        rider_well(),
+        bike_well(),
         fluidRow(
           column(
             12,
-            sliderInput("rider_weight", label = "Rider Weight:", min = 80, max = 250, value = c(150), width = slider_width))
-        ),
-        fluidRow(
-          column(
-            12,
-            sliderInput("bike_weight", label = "Bike Weight:", min = 10, max = 50, value = c(15), width = slider_width))
-        ),
-        fluidRow(
-          column(
-            12,
-            sliderInput("load_weight", label = "Load Weight:", min = 0, max = 72, value = c(13), width = slider_width))
-        ),
-        fluidRow(
-          column(
-            12,
-            sliderInput("front_distribution", label = "Front Wheel Load (%):",
-                        min = 35, max = 70, step = 5, value = c(40), width = slider_width))
-        ),
-        fluidRow(
-          column(
-            12,
-            wellPanel(
-              fluidRow(
-                column(12, h5('Tire Sizes (mm)', align = "left", style="font-weight: bold;"))
-              ),
-              fluidRow(
-                column(
-                  4,
-                  selectInput("front_tire", label = "Front:",
-                              choices = btpress::tire_sizes_mm, selected = 28,
-                              width = dropdown_width, selectize = FALSE)
-                ),
-                column(
-                  8,
-                  checkboxInput("front_casing_extralight", "Extra Light Casing", FALSE))
-              ),
-              fluidRow(
-                column(
-                  4,
-                  selectInput("rear_tire", label = 'Rear:',
-                              choices = btpress::tire_sizes_mm, selected = 28,
-                              width = dropdown_width, selectize = FALSE)),
-                column(
-                  8,
-                  checkboxInput("rear_casing_extralight", "Extra Light Casing", FALSE)
-                )
-              )
-            ) # wellPanel
+            tire_well()
           )
         )
       ),
